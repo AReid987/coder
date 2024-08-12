c69e2c2a2c036da45e7f8aaf3360d0c4201a7f97
Ethan Dickson <ethan@coder.com>
2024-08-12 13:29:38 +0000

    fixup

--- cli/completion.go
+++ cli/completion.go
@@ -1,7 +1,7 @@
 package cli
 
 import (
-	"golang.org/x/xerrors"
+	"fmt"
 
 	"github.com/coder/coder/v2/cli/cliui"
 	"github.com/coder/serpent"
@@ -31,26 +31,30 @@ func (*RootCmd) completion() *serpent.Command {
 		},
 		Handler: func(inv *serpent.Invocation) error {
 			if shellName != "" {
-				shell := completion.ShellByName(shellName, inv.Command.Parent.Name())
-				if shell == nil {
-					return xerrors.Errorf("unsupported shell %q", shellName)
+				shell, err := completion.ShellByName(shellName, inv.Command.Parent.Name())
+				if err != nil {
+					return err
+				}
+				if printOutput {
+					return shell.WriteCompletion(inv.Stdout)
 				}
 				return installCompletion(inv, shell)
 			}
-			// shell, err := completion.DetectUserShell(inv.Command.Parent.Name())
-			// if err == nil {
-			// 	return installCompletion(inv, shell)
-			// }
+			shell, err := completion.DetectUserShell(inv.Command.Parent.Name())
+			if err == nil {
+				return installCompletion(inv, shell)
+			}
+			// Silently continue to the shell selection if detecting failed.
 			choice, err := cliui.Select(inv, cliui.SelectOptions{
-				Message: "Select a shell to install completion for",
+				Message: "Select a shell to install completion for:",
 				Options: shellOptions.Choices,
 			})
 			if err != nil {
 				return err
 			}
-			shellChoice := completion.ShellByName(choice, inv.Command.Parent.Name())
-			if shellChoice == nil {
-				return xerrors.Errorf("unsupported shell %q", shellName)
+			shellChoice, err := completion.ShellByName(choice, inv.Command.Parent.Name())
+			if err != nil {
+				return err
 			}
 			if printOutput {
 				return shellChoice.WriteCompletion(inv.Stdout)
@@ -66,13 +70,17 @@ func installCompletion(inv *serpent.Invocation, shell completion.Shell) error {
 		// If we can't determine the install path, just print the completion script.
 		return shell.WriteCompletion(inv.Stdout)
 	}
+	action := "appending to"
+	if shell.UsesOwnFile() {
+		action = "creating"
+	}
 	choice, err := cliui.Select(inv, cliui.SelectOptions{
 		Options: []string{
 			"Confirm",
 			"Print to terminal",
 			"Cancel",
 		},
-		Message:    "Install completion for " + shell.Name() + " by appending to " + path + "?",
+		Message:    fmt.Sprintf("Install completion for %s by %s %s?", shell.Name(), action, path),
 		HideSearch: true,
 	})
 	if err != nil {
@@ -81,7 +89,7 @@ func installCompletion(inv *serpent.Invocation, shell completion.Shell) error {
 	if choice == "Cancel" {
 		return nil
 	}
-	if choice != "Print to terminal" {
+	if choice == "Print to terminal" {
 		return shell.WriteCompletion(inv.Stdout)
 	}
 	return completion.InstallShellCompletion(shell)
